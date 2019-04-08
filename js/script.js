59a92a51206455348d4a26a1992463df7127617b
DWave3239 <marvin.hengster@gmx.at>
2019-04-08 18:14:56 +0200

    Menu now works as intended + underlay

--- js/script.js
+++ js/script.js
@@ -11,40 +11,51 @@ function changeRotation(element){
 function moveNav(direction) {
     var elem    = document.getElementById('sidenav'),
         header  = document.getElementsByTagName('header')[0],
+        underlay= document.getElementById('underlay'),
         stop    = header.getBoundingClientRect().left, 
-        step    = 10, 
-        pos     = 0,
-        id      = setInterval(frame, 0.01);
-    
+        step    = 20,
+        pos     = elem.getBoundingClientRect().left,
+        id      = null;
+
     if(direction === 'in'){
-        pos = stop;
+        pos  = stop;
         stop = -elem.offsetWidth;
         step = -step;
+        id   = setInterval(frameOut, 1);
+        underlay.style.display = 'none';
+    }else{
+        id   = setInterval(frameIn, 1);
+        underlay.style.display = 'block';
     }
 
-    function frame() {
-        if(direction === 'in'){
-            if (pos <= stop) {
-                clearInterval(id);
+    function frameIn() {
+        if (pos >= stop) {
+              clearInterval(id);
+        } else {
+            if(pos + step >= stop){
+                pos = stop;
             }else{
-                if(pos+step <= stop){
-                    elem.style.left = stop + 'px';
-                }else{
-                    pos += step;
-                    elem.style.left = pos + 'px';
-                }
+                pos += step;
             }
-        }else{ // direction === 'out'
-            if (pos >= stop) {
-                clearInterval(id);
-            }else{
-                if(pos+step >= stop){
-                    elem.style.left = stop + 'px';
-                }else{
-                    pos += step;
-                    elem.style.left = pos + 'px';
-                }
-            }
+            elem.style.left = pos + 'px';
         }
     }
+
+    function frameOut(){
+        if (pos <= stop) {
+            clearInterval(id);
+        } else {
+            if(pos + step <= stop){
+                pos = stop;
+            }else{
+                pos += step;
+            }
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+function initPageLayout(){
+    var elem = document.getElementById('sidenav');
+    elem.style.left = -elem.offsetWidth + 'px';
 }
