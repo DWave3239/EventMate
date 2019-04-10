681c20e17caa4aedded1d54807118c2e17046cfa
DWave3239 <marvin.hengster@gmx.at>
2019-04-10 16:18:24 +0200

    Nav movement speed now dependent on window width

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function moveNav(direction) {
         header  = document.getElementsByTagName('header')[0],
         underlay= document.getElementById('underlay'),
         stop    = header.getBoundingClientRect().left, 
-        step    = 5,
+        step    = 5 + getPageWidth()/1920 * 15,
         pos     = elem.getBoundingClientRect().left;
 
     if(intervalId){
@@ -65,4 +65,42 @@ function moveNav(direction) {
 function initPageLayout(){
     var elem = document.getElementById('sidenav');
     elem.style.left = -elem.offsetWidth + 'px';
+
+    checkPageSize();
+}
+
+function getPageWidth(){
+    if (typeof window.innerWidth != 'undefined')
+        return window.innerWidth;
+    if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+        return document.documentElement.clientWidth;
+    return document.getElementsByTagName('body')[0].clientWidth;
+}
+
+function getPageHeight(){
+    if (typeof window.innerWidth != 'undefined')
+        return window.innerHeight;
+    if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+        return document.documentElement.clientHeight;
+    return document.getElementsByTagName('body')[0].clientHeight;
+}
+
+function checkPageSize(){
+    var viewportwidth;
+    var viewportheight;
+    
+    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+    
+    if (typeof window.innerWidth != 'undefined'){
+        viewportwidth = window.innerWidth;
+        viewportheight = window.innerHeight;
+    }else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0){ // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+        viewportwidth = document.documentElement.clientWidth;
+        viewportheight = document.documentElement.clientHeight;
+    }else{ // older versions of IE
+        viewportwidth = document.getElementsByTagName('body')[0].clientWidth;
+        viewportheight = document.getElementsByTagName('body')[0].clientHeight;
+    }
+
+    console.log(viewportwidth + ", " + viewportheight);
 }
