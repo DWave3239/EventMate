0579f6607317b7da8554bdd07a4e573116d8e8a1
k01457866 <SimonEilmsteiner95@gmail.com>
2019-04-15 11:46:11 +0200

    Distance calculation done

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,8 +1,8 @@
-var x, message, lat, long;
+var x, message, lat, lon;
 function getLocation() {
   x = document.getElementById("map");
   lat = 0;
-  long = 0;
+  lon = 0;
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
@@ -14,7 +14,9 @@ function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
     lat = position.coords.latitude;
-    long = position.coords.longitude;
+    lon = position.coords.longitude;
+    
+    distanceInKmBetweenEarthCoordinates(0,0);
 }
 
 function showError(error) {
@@ -35,6 +37,28 @@ function showError(error) {
     iqwerty.toast.Toast(message);
 } 
 
-function calcDistance(latEvent, longEvent) {
-    
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+  
+function distanceInKmBetweenEarthCoordinates(lat2, lon2) {
+    var earthRadiusKm = 6371;
+  
+    var dLat = degreesToRadians(lat2-lat);
+    var dLon = degreesToRadians(lon2-lon);
+  
+    var lat1 = degreesToRadians(lat);
+    lat2 = degreesToRadians(lat2);
+  
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    iqwerty.toast.Toast(earthRadiusKm * c + 'km');
+    return earthRadiusKm * c;
+}
+
+function setLocation(newLat, newLon) { 
+    //if locationing does not work it is possible to set the coordinates manualy
+    lat = newLat;
+    lon = newLon;
 }
