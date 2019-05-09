687ae7a5df605354bb6ec2fcd900268964ab025a
DWave3239 <marvin.hengster@gmx.at>
2019-05-09 13:26:37 +0200

    sidenav css

--- js/swipe.js
+++ js/swipe.js
@@ -1,4 +1,4 @@
-document.addEventListener('touchstart', handleTouchStart, false);        
+//document.addEventListener('touchstart', handleTouchStart, false);        
 document.addEventListener('touchmove', handleTouchMove, false);
 
 var xDown = null;                                                        
@@ -9,13 +9,13 @@ function getTouches(evt) {
          evt.originalEvent.touches; // jQuery
 }                           
 
-function handleTouchStart(evt) {
+/*function handleTouchStart(evt) {
     const firstTouch = getTouches(evt)[0];                                      
     xDown = firstTouch.clientX;                                      
     yDown = firstTouch.clientY;
     
     document.getElementById('footer').innerHTML = "<p>Detected touch!</p>";
-};                                                
+};*/                                     
 
 function handleTouchMove(evt) {
     if(!xDown || !yDown){
